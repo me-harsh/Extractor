c2d263303620bbb5479bb44ec876f4c0a61b6859
Harsh Raj <116838873+me-harsh@users.noreply.github.com>
2025-08-11 10:35:49 +0530

    just adding the convert docx to pdf function

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from src.pdf_processing import load_pdf_with_fitz
 from src.llm_client import LocalLLMClient
 from src.pattern_extractor import EnhancedPatternExtractor
 from src.question_processor import process_all_pdfs_with_verification
-
+from convert_doc_to_pdf1 import convert_all_docx_in_test_files #check if this works
 def main():
     """Main entry point for the question extraction pipeline"""
+    convert_all_docx_in_test_files()# keep this for chaning the docx to pdf
     output = process_all_pdfs_with_verification(model="ibnzterrell/Meta-Llama-3.3-70B-Instruct-AWQ-INT4", 
                                      base_url="http://localhost:9017/v1")
     
